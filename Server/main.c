6c92966bd8798ba45bdbe08d5c5e8b466f1bee4f
SillyCatEnthusiast <wfelkay@gmail.com>
2025-04-30 16:34:15 -0400

    multiple connectivity v2
    All i need to do is add the multithreading

--- Server/main.c
+++ Server/main.c
@@ -10,12 +10,9 @@ int main(){
     Node* head = NULL;
 
     Message *msg = malloc(sizeof(Message));
+
     int server_fd; 
-    // int client_fd;
     struct sockaddr_in server_addr; // creates a sockaddr_in for handling incoming connections 
-    // struct sockaddr_in client_addr; // creates a sockaddr_in for maintaining a connection to a specific client
-    // socklen_t addr_size; // just a fancier int 
-    // char buffer[1024] = {0}; // basically just calloc
     server_fd = socket(AF_INET, SOCK_STREAM, 0); // tells it that its looking for an ipv4 tcp
     server_addr.sin_family = AF_INET; // tells it that it is an ipv4 server
     server_addr.sin_addr.s_addr = INADDR_ANY; // allowed ips: 0.0.0.0 (all)
@@ -23,17 +20,16 @@ int main(){
     bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
     // typecasts server_addr into a regular sockaddr and binds the connector to the server file descriptor
     listen(server_fd, 5); // listens for up to 5 queued requests
-    // addr_size = sizeof(client_addr);
     while (1){
     Node *n = newNode();
     addNode(&head, n);
 
     n -> client_fd = accept(server_fd, (struct sockaddr*)&n -> client_addr, &n -> addr_size); // binds the client
-    void printList(Node* head);
+    recieveText(n -> client_fd, msg);
+    printf("Received: %s\n", msg -> text);
     }
     // while (1){
-    // recieveText(client_fd, msg);
-    // printf("Received: %s\n", msg -> text);
+
     // servSendText(client_fd, msg);
     // }
     // close(client_fd);
